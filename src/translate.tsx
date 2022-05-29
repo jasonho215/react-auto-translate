e4768311e586f984974fb33cd322893a0cd9cc72
Jason Ho <jasonho215@gmail.com>
2022-05-30 03:01:08 +0800

    feat: Add onTranslated callback in Translate component

--- src/translate.tsx
+++ src/translate.tsx
@@ -1,17 +1,27 @@
-import React, {useContext, useState} from 'react';
+import React, {useCallback, useContext, useState} from 'react';
 import {TranslateContext, LanguageContext} from './translator';
 
 export default function Translate({
   children: value,
+  onTranslated,
 }: {
   children: string;
+  onTranslated?: (translation: string) => void;
 }): JSX.Element {
   const language = useContext(LanguageContext);
   const handleTranslate = useContext(TranslateContext);
   const [translation, setTranslation] = useState(value);
 
+  const handleTranslation = useCallback(
+    (translated: string) => {
+      setTranslation(translated);
+      onTranslated && onTranslated(translated);
+    },
+    [onTranslated]
+  );
+
   React.useEffect(() => {
-    handleTranslate(value, setTranslation);
+    handleTranslate(value, handleTranslation);
   }, [value, language]);
 
   return <>{translation}</>;
